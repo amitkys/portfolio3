06612007d41b3562b46300e70329ed8d1819967a
KYs <amitkys59@gmail.com>
2025-02-27 00:59:35 +0530

    fix prettier issue

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,12 @@
 import { Spotlight } from "@/components/ui/spotlight";
 
 export default function Page() {
-    return (
-        <div className="flex min-h-screen items-center justify-center overflow-hidden">
-             <Spotlight
+  return (
+    <div className="flex min-h-screen items-center justify-center overflow-hidden">
+      <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-        </div>
-    )
+    </div>
+  );
 }
