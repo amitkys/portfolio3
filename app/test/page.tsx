ceda0e7005a550f57e055ea953aa5959af8f5dbf
KYs <amitkys59@gmail.com>
2025-04-05 02:38:46 +0530

    revert v4 to v3

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1>Test Page</h1>
+      <h1>test page change to revert tailwind v3</h1>
     </div>
-  )
+  );
 }
